b0cb6a92f5c3d726a1d4801da66c22ed1629a28a
Alriightyman <alriightyman@gmail.com>
2022-08-05 11:20:47 -0400

    Added FileItem Rename

--- src/MDStudioPlus/Models/FileExplorer/Item.cs
+++ src/MDStudioPlus/Models/FileExplorer/Item.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace MDStudioPlus.FileExplorer
 {
@@ -13,8 +14,31 @@ namespace MDStudioPlus.FileExplorer
     /// </summary>
     public abstract class Item : ViewModelBase
     {
+        private RelayCommand renameCommand;
+        private RelayCommand lostFocusCommand;
+        private string name = "";
+        protected string oldName;
         public event SelectedItemEventHandler OnSelectedItemChanged;
-        public string Name { get; set; }
+        public string Name
+        {
+            get => name;
+            set
+            {
+                if (name != value && value != String.Empty)
+                {
+                    oldName = name;
+                    name = value;
+
+                    // when this is first set, oldName should be the same as Name
+                    if(oldName == String.Empty)
+                    {
+                        oldName = name;
+                    }
+
+                    RaisePropertyChanged(nameof(Name));
+                }
+            }
+        }
         public string Path { get; set; }
         public Item Parent { get; set; }
 
@@ -33,6 +57,58 @@ namespace MDStudioPlus.FileExplorer
                     OnSelectedItemChanged(this, new SelectedItemEventArgs(this));
                 }
             }
+        }
+
+        private bool isRenaming;
+
+        public bool IsRenaming
+        {
+            get => isRenaming;
+            set
+            {
+                isRenaming = value;
+                RaisePropertyChanged(nameof(IsRenaming));
+            }
+        }
+
+
+        public ICommand RenameCommand
+        {
+            get
+            {
+                if (renameCommand == null)
+                {
+                    renameCommand = new RelayCommand((p) => OnRename());
+                }
+                return renameCommand;
+            }
+        }
+        
+
+        public ICommand LostFocusCommand
+        {
+            get
+            {
+                if (lostFocusCommand == null)
+                {
+                    lostFocusCommand = new RelayCommand((p) => OnLostFocus(p));
+                }
+                return lostFocusCommand;
+            }
+        }
+        
+        public Item()
+        {
+        }
+
+        private void OnRename()
+        {
+            IsRenaming = true;
+        }
+
+        protected virtual void OnLostFocus(object param)
+        {
+            IsRenaming = false;
         }
     }
 }
